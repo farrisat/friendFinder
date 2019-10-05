6357ef1ef8f5dd5d17b0f358c8524c6f7220fe08
Alex F <farrisat1@gmail.com>
2019-10-04 19:24:51 -0700

    Completed basic api routes. Need to add compatability logic still.

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,26 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var friendsData = require("../data/friends");
+
+
+
+
+// ROUTING
+module.exports = function(app) {
+  // API GET Requests
+  app.get("/api/friends", function(req, res) {
+    res.json(friendsData);
+  });
+
+
+  // API POST Requests
+  app.post("/api/friends", function(req, res) {
+
+      res.json(friendsData);
+    
+    });
+}
